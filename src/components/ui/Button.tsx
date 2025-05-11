5359da66a9ed90293752866c15948910244e8937
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-05-11 23:42:33 +0530

    Clean up and CLI setup

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='bg-red-500 rounded-md px-4 py-2'>
+        Click
+      </button>
+    </div>
+  )
+}
+
+export default Button
