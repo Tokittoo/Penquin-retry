a0be80b8a9a42282ea0895c851046fb7911c7a0b
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-05-14 10:59:43 +0530

    Added playground features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,22 @@ export default function Home() {
       <div className='flex flex-col gap-4 justify-between items-center'>
         {/* Playground -> */}
 
+        
+        <div className="flex gap-2">
+          <div className='bg-primary text-primary-foreground p-4 rounded-md'>
+            Primary
+          </div>
+          <div className='bg-secondary text-secondary-foreground p-4 rounded-md'>
+            Primary
+          </div>
+          <div className='bg-muted text-muted-foreground p-4 rounded-md'>
+            Primary
+          </div>
+          <div className='bg-accent text-accent-foreground p-4 rounded-md'>
+            Primary
+          </div>
+        </div>
+
       </div>
     </div>
   );
