643e02a5631fa89f84030e75030a28f0b5023b1d
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-05-19 23:56:46 +0530

    fix: Headline

--- src/registry/components/ui/headline.tsx
+++ src/registry/components/ui/headline.tsx
@@ -7,6 +7,7 @@ const Headline = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>)
       className={cn(
         'bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-600 dark:from-neutral-600 dark:to-white',
         'text-5xl md:text-7xl py-2 relative z-20 font-bold font-sans tracking-tight',
+        className
       )}
       {...props}
     />
