31f783c15a504bec95cbcfa1ab417cc86e1c2da0
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-05-19 23:22:36 +0530

    Add: Headline

---
+++ src/registry/components/ui/headline.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+const Headline = ({...props}: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <h1
+      className={cn(
+        'bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-600 dark:from-neutral-600 dark:to-white',
+        'text-5xl md:text-7xl py-2 md:py-10 relative z-20 font-bold font-sans tracking-tight'
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Headline }
