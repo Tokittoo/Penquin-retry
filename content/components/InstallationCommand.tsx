adb92b4ae53911c7bf8693923421d87b97ad7ee2
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-06-05 17:11:41 +0530

    Docs: Updated to use Vink cli

--- content/components/InstallationCommand.tsx
+++ content/components/InstallationCommand.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from 'fumadocs-ui/components/tabs'
 import { Pre, CodeBlock } from 'fumadocs-ui/components/codeblock'
 
-export const InstallationCommand = ({ dirPath }: { dirPath: string }) => {
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL ?? ''
+export const InstallationCommand = ({ component, command }: { component?: string, command?: string }) => {
+  if(!command && ! component) throw new Error('Atleast one param is required in InstalltionCommand');
 
   return (
     <Tabs defaultValue="npm">
@@ -18,32 +18,40 @@ export const InstallationCommand = ({ dirPath }: { dirPath: string }) => {
 
       <TabsContent value="npm">
         <CodeBlock>
-          <Pre className='px-6 font-mono'>
-            npx shadcn@latest add {baseURL}/registry{dirPath}
+          <Pre className='px-6 font-mono text-[14px]'>
+            {
+              command ? `npx vink ${command}` : `npx vink add ${component}`
+            }
           </Pre>
         </CodeBlock>
       </TabsContent>
 
       <TabsContent value="yarn">
         <CodeBlock lang="bash" keepBackground>
-          <Pre className='px-6 font-mono'>
-            yarn dlx shadcn@latest add {baseURL}/registry{dirPath}
+          <Pre className='px-6 font-mono text-[14px]'>
+            {
+              command ? `yarn vink ${command}` : `yarn vink add ${component}`
+            }
           </Pre>
         </CodeBlock>
       </TabsContent>
 
       <TabsContent value="pnpm">
         <CodeBlock lang="bash" keepBackground>
-          <Pre className='px-6 font-mono'>
-            pnpm dlx shadcn@latest add {baseURL}/registry{dirPath}
+          <Pre className='px-6 font-mono text-[14px]'>
+            {
+              command ? `pnpm dlx vink ${command}` : `pnpm dlx vink add ${component}`
+            }
           </Pre>
         </CodeBlock>
       </TabsContent>
 
       <TabsContent value="bun">
         <CodeBlock lang="bash" keepBackground>
-          <Pre className='px-6 font-mono'>
-            bunx --bun shadcn@latest add {baseURL}/registry{dirPath}
+          <Pre className='px-6 font-mono text-[14px]'>
+            {
+              command ? `bunx --bun vink ${command}` : `bunx --bun vink add ${component}`
+            }
           </Pre>
         </CodeBlock>
       </TabsContent>
